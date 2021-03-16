3b3ea7da9c73a2bdce9657d1e209123893ca4584
sivarajsundar <sivarajsundaresan@gmail.com>
2021-03-16 19:36:04 +0530

    application.rb file

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,8 @@ module CoromandelHarleyDavidson
       # YAML::ENGINE.yamler = 'syck'
 
       config.active_job.queue_adapter = :delayed_job
-      config.assets.initialize_on_precompile = false
+      config.assets.initialize_on_precompile = true
+
 
       config.api_only = false
 
