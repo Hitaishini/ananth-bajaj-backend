44023025017e267c3294fa531e2e561a0743f370
sivarajsundar <sivarajsundaresan@gmail.com>
2021-03-16 18:52:49 +0530

    application.rb changes

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'csv'
+require 'yaml'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -33,6 +34,7 @@ module CoromandelHarleyDavidson
         g.javascripts = false
         g.helper = false
       end
+      YAML::ENGINE.yamler = 'syck'
 
       config.active_job.queue_adapter = :delayed_job
 
